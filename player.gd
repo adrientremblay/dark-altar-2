a0f55655b787a6642692a52e32b45195d507d6d7
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-18 19:34:04 -0400

    Basic first person camera

--- player.gd
+++ player.gd
@@ -2,6 +2,20 @@ extends CharacterBody3D
 
 @export var speed = 20
 
+@onready var neck = $CameraPivot
+@onready var camera = $CameraPivot/Camera3D
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * 0.01)
+			neck.rotate_x(-event.relative.y * 0.01)
+
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		position.x += speed * delta
