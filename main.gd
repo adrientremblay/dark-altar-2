6e6074dc046a5a1ab4f46f29f2bd2b4f5e30c189
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-18 10:53:02 -0400

    Mobs spawn

--- main.gd
+++ main.gd
@@ -1,2 +1,14 @@
 extends Node3D
 
+@export var mob_scene: PackedScene
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	var player_position = $Player.position
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	add_child(mob)
