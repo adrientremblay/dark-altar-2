c395120eff9bbb2f1e38dc4158636d8587d93742
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-18 15:09:00 -0400

    implemented jumping

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
 
 var target_velocity = Vector3.ZERO
 
@@ -28,6 +29,9 @@ func _physics_process(delta):
 	# Vertical Velocity
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 	
 	velocity = target_velocity
 	move_and_slide()
