4cd51d85732a17ec6875446f6c93f513646d8814
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-22 18:56:24 -0400

    Monster follows at a distance and faces player

--- cedric.gd
+++ cedric.gd
@@ -1,4 +1,8 @@
 extends CharacterBody3D
 
+var random = RandomNumberGenerator.new()
+
 func teleport(player_position):
-	position = player_position
+	var random_distance_vector = Vector3(random.randf_range(1, 2), 0, random.randf_range(1, 2))
+	position = player_position + random_distance_vector
+	rotate(position, position.angle_to(player_position))
