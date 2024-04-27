a10fc80c951a4eb1a0d0242e63a70bb8c902314d
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-27 13:42:53 -0400

    Cedric cannot tp if you are looking right at him

--- main.gd
+++ main.gd
@@ -19,7 +19,7 @@ func _process(delta: float) -> void:
 		var angle_scale = ((75 - angle_to_cedric) / 75) * 0.5
 		var distance_scale = ((10 - distance_to_cedric) / 10) * 0.5
 		var damage_scale = angle_scale + distance_scale
-		$Player.health -= damage_scale * 2.0
+		$Player.health -= damage_scale * 0.5
 	elif $Player.health < 100:
 		$Player.health += 1
 		
@@ -38,5 +38,15 @@ func _process(delta: float) -> void:
 	shaderMat.set_shader_parameter("amplitude", amplitude)
 
 func _on_teleport_timer_timeout() -> void:
+	var angle_to_cedric = abs($Player.check_if_can_see_me($Cedric))
+	
+	var distance_to_cedric_vec = $Player.global_position - $Cedric.global_position
+	var distance_to_cedric = distance_to_cedric_vec.length()
+	
+	var ghost = 0
+	var amplitude = 0.01
+	if (angle_to_cedric < 75 && distance_to_cedric <= 10):
+		return
+	
 	var player_position = $Player.position
 	$Cedric.teleport(player_position)
