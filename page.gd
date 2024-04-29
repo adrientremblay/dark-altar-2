bec08010e679967002315ac5d21b24b395f3dfe0
Adrien Tremblay <adrien@adrientremblay.com>
2024-04-28 20:57:18 -0400

    tweaked page title layout

--- page.gd
+++ page.gd
@@ -1,4 +1,4 @@
 class_name Page extends Area3D
 
 @export var page_title : String = "Page Title"
-@export var page_text : String = "Page Text"
+@export_multiline var page_text : String = "Page Text"
