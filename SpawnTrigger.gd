c7141bc527e5b14face29ceaa6e68d1c1183cb89
Adrien Tremblay <adrien@adrientremblay.com>
2024-08-16 22:32:04 -0400

    Implemented deleting the scene once player leaves the spawn area

--- SpawnTrigger.gd
+++ SpawnTrigger.gd
@@ -4,8 +4,12 @@ var spawned_level
 
 @export var level: PackedScene
 
-
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		spawned_level = level.instantiate()
 		add_child(spawned_level)
+
+func _on_body_exited(body: Node3D) -> void:
+	if spawned_level:
+		spawned_level.queue_free()
+		spawned_level = null
