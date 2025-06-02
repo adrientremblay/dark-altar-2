8f3cc12f27a33f314489f8abdbb7ee1363d909b5
Adrien Tremblay <adrien@adrientremblay.com>
2025-06-01 20:16:11 -0400

    Add an intro label

--- scenes/ui/UI.gd
+++ scenes/ui/UI.gd
@@ -5,6 +5,9 @@ var intro_label = 1
 func _on_intro_timer_timeout() -> void:
 	match intro_label:
 		1:
+			$BOOM.play()
+			$Label.visible = true
+		2:
 			$IntroTimer.stop()
 			$Label.visible = false
 	intro_label +=1
