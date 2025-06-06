f7e2f4110a71efd35e0c65f15a5132eb910ec8ed
Adrien Tremblay <adrien@adrientremblay.com>
2025-06-05 22:04:20 -0400

    Basis of spawn traps

---
+++ spawn_trap.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+@export var teleport_target: Node3D
+@export var cedric: Cedric
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		cedric.global_position = teleport_target.global_position
+		print("TPing cedric to " + str (teleport_target.global_position))
