b4ab7bfbcf76abe471ac0a35470ef5f38866889a
Adrien Tremblay <adrien@adrientremblay.com>
2025-07-01 18:43:47 -0400

    Switching for ambient tracks

--- ambient_sound.gd
+++ ambient_sound.gd
@@ -1,21 +1,37 @@
 extends Node
 
-var in_dungeon = false
-
-func _on_gate_to_hell_body_exited(body: Node3D) -> void:
-	in_dungeon = not in_dungeon
+func _on_dungeon_audio_switcher_body_entered(body: Node3D) -> void:
+	if (not body.is_in_group("player")):
+		return
 	
-	if (in_dungeon):
-		$DungeonAmbience.play()
-		$NightBugs.stop()
-		
-		$Cricket.enabled = false
-		$Owl.enabled = false
-		$NightBird.enabled = false
-	else:
-		$DungeonAmbience.stop()
-		$NightBugs.play()
-		
-		$Cricket.enabled = true
-		$Owl.enabled = true
-		$NightBird.enabled = true
+	$DungeonAmbience.play()
+	$NightBugs.stop()
+	$WindAmbience.stop()
+	
+	$Cricket.enabled = false
+	$Owl.enabled = false
+	$NightBird.enabled = false
+
+func _on_outside_audio_switcher_body_entered(body: Node3D) -> void:
+	if (not body.is_in_group("player")):
+		return
+	
+	$DungeonAmbience.stop()
+	$NightBugs.play()
+	$WindAmbience.stop()
+	
+	$Cricket.enabled = true
+	$Owl.enabled = true
+	$NightBird.enabled = true
+
+func _on_church_audio_switcher_2_body_entered(body: Node3D) -> void:
+	if (not body.is_in_group("player")):
+		return
+	
+	$DungeonAmbience.stop()
+	$NightBugs.stop()
+	$WindAmbience.play()
+	
+	$Cricket.enabled = false
+	$Owl.enabled = false
+	$NightBird.enabled = false
