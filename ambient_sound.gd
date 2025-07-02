135e19bca130ec980df3d503e415729f69135b9d
Adrien Tremblay <adrien@adrientremblay.com>
2025-07-01 20:01:12 -0400

    Get audio fading to work

--- ambient_sound.gd
+++ ambient_sound.gd
@@ -1,13 +1,28 @@
 extends Node
 
+@onready var tween1 :Tween
+@onready var tween2 :Tween
+
+var MIN_VOLUME = -80 #db
+var MAX_VOLUME = 0 #db
+var TRANSITION_TIME = 2 #seconds
+
 func _on_dungeon_audio_switcher_body_entered(body: Node3D) -> void:
 	if (not body.is_in_group("player")):
 		return
-	
+		
 	$DungeonAmbience.play()
+	$DungeonAmbience.volume_db = MIN_VOLUME
+	var tween1 = get_tree().create_tween().set_parallel(true)
+	tween1.tween_property($DungeonAmbience, "volume_db", MAX_VOLUME, TRANSITION_TIME).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+
+	$ChurchAmbience.play()
+	$ChurchAmbience.volume_db = MAX_VOLUME
+	var tween2 = get_tree().create_tween().set_parallel(true)
+	tween2.tween_property($ChurchAmbience, "volume_db", MIN_VOLUME, TRANSITION_TIME).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+
 	$NightBugs.stop()
-	$WindAmbience.stop()
-	
+
 	$Cricket.enabled = false
 	$Owl.enabled = false
 	$NightBird.enabled = false
@@ -16,9 +31,17 @@ func _on_outside_audio_switcher_body_entered(body: Node3D) -> void:
 	if (not body.is_in_group("player")):
 		return
 	
-	$DungeonAmbience.stop()
 	$NightBugs.play()
-	$WindAmbience.stop()
+	$NightBugs.volume_db = MIN_VOLUME
+	var tween1 = get_tree().create_tween().set_parallel(true)
+	tween1.tween_property($NightBugs, "volume_db", MAX_VOLUME, TRANSITION_TIME).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+
+	$ChurchAmbience.play()
+	$ChurchAmbience.volume_db = MAX_VOLUME
+	var tween2 = get_tree().create_tween().set_parallel(true)
+	tween2.tween_property($ChurchAmbience, "volume_db", MIN_VOLUME, TRANSITION_TIME).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	
+	$DungeonAmbience.stop()
 	
 	$Cricket.enabled = true
 	$Owl.enabled = true
@@ -28,9 +51,19 @@ func _on_church_audio_switcher_2_body_entered(body: Node3D) -> void:
 	if (not body.is_in_group("player")):
 		return
 	
-	$DungeonAmbience.stop()
-	$NightBugs.stop()
-	$WindAmbience.play()
+	$ChurchAmbience.play()
+	$ChurchAmbience.volume_db = MIN_VOLUME
+	var tween1 = get_tree().create_tween().set_parallel(true)
+	tween1.tween_property($ChurchAmbience, "volume_db", MAX_VOLUME, TRANSITION_TIME).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+
+	var playingAudio: AudioStreamPlayer
+	if $DungeonAmbience.playing:
+		playingAudio = $DungeonAmbience
+	else:
+		playingAudio = $NightBugs
+
+	var tween2 = get_tree().create_tween().set_parallel(true)
+	tween2.tween_property(playingAudio, "volume_db", MIN_VOLUME, TRANSITION_TIME).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 	
 	$Cricket.enabled = false
 	$Owl.enabled = false
