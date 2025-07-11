3a18accaad58063d7686f622d53e973e0eb02af8
Adrien Tremblay <adrien@adrientremblay.com>
2025-07-11 16:35:03 -0400

    placed cross pickups

--- level.gd
+++ level.gd
@@ -1,8 +1,6 @@
 class_name Level extends Node3D
 
-@export var skull_group: SkullGroup
 signal skull_collected
 
 func init(skull_keep_index: int):
-	if skull_group:
-		skull_group.init(skull_keep_index)
+	pass
