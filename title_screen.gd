7aec39d6d5d49ff89887af206371e4e8be814b77
Adrien Tremblay <adrien@adrientremblay.com>
2025-07-12 18:02:51 -0400

    Added a sound when buttons are pressed

--- title_screen.gd
+++ title_screen.gd
@@ -6,10 +6,12 @@ func _on_start_game_button_pressed() -> void:
 func _on_controls_button_pressed() -> void:
 	$UI/Menus/Lore.visible = false
 	$UI/Menus/Controls.visible = true
+	$ButtonPressSound.play()
 
 func _on_lore_button_pressed() -> void:
 	$UI/Menus/Lore.visible = true
 	$UI/Menus/Controls.visible = false
+	$ButtonPressSound.play()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
