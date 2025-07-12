f4883a945d2436c25dcb232a60b0809ee48a909a
Adrien Tremblay <adrien@adrientremblay.com>
2025-07-12 17:45:59 -0400

    Implemented buttons

--- title_screen.gd
+++ title_screen.gd
@@ -1,5 +1,15 @@
 extends Node3D
 
-
 func _on_start_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn");
+
+func _on_controls_button_pressed() -> void:
+	$UI/Menus/Lore.visible = false
+	$UI/Menus/Controls.visible = true
+
+func _on_lore_button_pressed() -> void:
+	$UI/Menus/Lore.visible = true
+	$UI/Menus/Controls.visible = false
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
