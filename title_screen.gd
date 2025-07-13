ac16e6e41e1d54d34533ead2241453985d409e6f
Adrien Tremblay <adrien@adrientremblay.com>
2025-07-12 23:23:34 -0400

    Implemented intro dialogue

--- title_screen.gd
+++ title_screen.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
 func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn");
+	$UI.visible = false
+	$Camera3D.queue_free()
+	$BlackPanel.visible = true
+	$MusicPlayer.stop()
+	$IntroDialogue.play()
 
 func _on_controls_button_pressed() -> void:
 	$UI/Menus/Lore.visible = false
@@ -15,3 +19,9 @@ func _on_lore_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func switch_level() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_intro_dialogue_finished() -> void:
+	switch_level()
