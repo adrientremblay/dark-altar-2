e6767f299cc33055b4186e997f06c0ac3128a788
Adrien Tremblay <adrien@adrientremblay.com>
2025-07-29 19:15:27 -0400

    Game done

--- scenes/ui/UI.gd
+++ scenes/ui/UI.gd
@@ -14,7 +14,7 @@ func display_message(message):
 func display_dialogue(message):
 	message_label.text = message
 	message_label.visible = true
-	message_timer.start()
+	message_timer.stop()
 
 func hide_message_label():
 	message_label.visible = false
